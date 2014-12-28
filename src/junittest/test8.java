b9c6197ee3425ac543fe269f4ac8fb36eb28b1cc
bent533 <bent533@gmail.com>
2014-12-28 18:23:06 +0400

    Update test8.java

--- src/junittest/test8.java
+++ src/junittest/test8.java
@@ -1,16 +1,12 @@
 /*
- * 8. Ввeсти логин с использованием букв РАЗНОГО регистра. Указать корректный пароль.
+ * 8. Р’РІeСЃС‚Рё Р»РѕРіРёРЅ СЃ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµРј Р±СѓРєРІ Р РђР—РќРћР“Рћ СЂРµРіРёСЃС‚СЂР°. РЈРєР°Р·Р°С‚СЊ РєРѕСЂСЂРµРєС‚РЅС‹Р№ РїР°СЂРѕР»СЊ.
  */
 package junittest;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import java.io.File;
 import java.io.IOException;
- 
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,20 +15,13 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
-
-/*С‡С‚РѕР±С‹ РІРєР»СЋС‡РёС‚СЊ С„Р»Р°Рі С‚РµСЃС‚*/
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
-/**С‡С‚РѕР±С‹ СѓСЃС‚Р°РЅРѕРІРёС‚СЊ РІСЂРµРјСЏ РЅР° РѕС‚РєСЂС‹С‚РёРµ Р±СЂР°СѓР·РµСЂР°**/
 import org.junit.*;
-
 import java.util.concurrent.TimeUnit;
-
-/******РїРѕРёСЃРє СЌР»РµРјРµРЅС‚Р° РЅР° СЃС‚СЂР°РЅРёС†Рµ  by id****/
 import org.openqa.selenium.*;
-
 
 
 
@@ -44,7 +33,7 @@ public class test8 {
      @BeforeClass
      public static void createAndStartService() throws IOException {
          service = new ChromeDriverService.Builder()
-            .usingDriverExecutable(new File("D://тестирование/chromedriver.exe"))
+            .usingDriverExecutable(new File("D://С‚РµСЃС‚РёСЂРѕРІР°РЅРёРµ/chromedriver.exe"))
             .usingAnyFreePort()
             .build();
          service.start();
@@ -55,11 +44,9 @@ public class test8 {
     	 DesiredCapabilities capabilities = DesiredCapabilities.chrome();
     	 ChromeOptions options = new ChromeOptions();
     	 options.addArguments("--test-type");
-    	 capabilities.setCapability("chrome.binary","D://тестирование/chromedriver.exe");
+    	 capabilities.setCapability("chrome.binary","D://С‚РµСЃС‚РёСЂРѕРІР°РЅРёРµ/chromedriver.exe");
     	 capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-    	    
     	 driver = new ChromeDriver(service, capabilities);
-     
     	 baseUrl = "http://www.moedelo.org/";
     	 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
      }
@@ -71,9 +58,9 @@ public class test8 {
     	 driver.findElement(By.id("signinEmail")).sendKeys("BeNt533@GMAil.cOm");
     	 driver.findElement(By.id("signinPassword")).clear();
     	 driver.findElement(By.id("signinPassword")).sendKeys("19910411");
-    	 driver.findElement(By.xpath("//input[@value='Войти в сервис']")).click();
-    	 driver.findElement(By.linkText("Бух. консультации")).click();
-    	 driver.findElement(By.linkText("Выйти")).click();
+    	 driver.findElement(By.xpath("//input[@value='Р’РѕР№С‚Рё РІ СЃРµСЂРІРёСЃ']")).click();
+    	 driver.findElement(By.linkText("Р‘СѓС…. РєРѕРЅСЃСѓР»СЊС‚Р°С†РёРё")).click();
+    	 driver.findElement(By.linkText("Р’С‹Р№С‚Рё")).click();
     	 driver.get(baseUrl + "/Default/Auth");
      }
  
